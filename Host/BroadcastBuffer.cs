77ceff420c7cee0aa9c6fe8e394fe180a531be7f
sampletext32 <sampletext32@bk.ru>
2020-06-29 22:57:46 +0300

    Move classes to separate files

---
+++ Host/BroadcastBuffer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace Host
+{
+    // буфер рассылки
+    public class BroadcastBuffer
+    {
+        // текущий буфер байт для последовательного доступа
+        public byte[] CurrentBuffer { get; private set; }
+
+        // позиция в текущем буфере
+        public int Position { get; private set; }
+
+        // очередь буферов рассылки
+        public Queue<byte[]> Queue { get; private set; }
+
+        // получение следующего байт к рассылке (связывает последовательно очередь буферов)
+        public byte GetByte()
+        {
+            // если текущий буфер пуст или мы достигли конца
+            if (CurrentBuffer == null || Position == CurrentBuffer.Length)
+            {
+                // если есть следующий буфер
+                if (Queue.Count > 0)
+                {
+                    // подставляем следующий буфер
+                    CurrentBuffer = Queue.Dequeue();
+                    // читаем сначала
+                    Position = 0;
+                    // вызываем рекурсивное получение следующего байта
+                    return GetByte();
+                }
+                else
+                {
+                    // новых буфферов нет, рассылать нечего, возвращаем 0 (тишина)
+                    CurrentBuffer = null;
+                    return 0;
+                }
+            }
+            else
+            {
+                // текущий буфер ещё актуален
+
+                // получаем следующий байт
+                byte data = CurrentBuffer[Position];
+                // сдвигаемся на следующий байт
+                Position++;
+                return data;
+            }
+        }
+
+        public BroadcastBuffer()
+        {
+            Queue = new Queue<byte[]>();
+        }
+    }
+}
---
+++ Host/ClientData.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Sockets;
+
+namespace Host
+{
+    // объект состояния клиента
+    public class ClientData
+    {
+        // сокет клиент
+        public Socket Socket { get; private set; }
+
+        // буфер приёма сокета
+        public byte[] SocketReceiveBuffer { get; private set; }
+
+        // буфер рассылки
+        public BroadcastBuffer BroadcastBuffer { get; private set; }
+
+        public ClientData(Socket socket, int size)
+        {
+            Socket = socket;
+            SocketReceiveBuffer = new byte[size];
+            BroadcastBuffer = new BroadcastBuffer();
+        }
+    }
+}
